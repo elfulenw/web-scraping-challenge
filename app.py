99efc39a5b4044d2962cd4a430ce7f9fc5baf2b6
elfulenw <elfulenw@gmail.com>
2020-09-28 20:21:37 -0400

    new scrape with updated files to submit

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ mongo = PyMongo(app,uri = 'mongodb://localhost:27017/mission_to_mars_db')
 @app.route('/')
 def index():
     mars_data = mongo.db.mars_information.find_one()
-    return render_template('index.html', data=mars_information)
+    return render_template('index.html', data=mars_data)
 
 @app.route('/scrape')
 def scrape():
-    scrape_mars = scrape_mars.scrape()
-    print(scrape_mars)
-    mongo.db.mars_information.update({}, scrape_mars, upsert=True)
+    mars_data = scrape_mars.scrape()
+    print(mars_data)
+    mongo.db.mars_information.update({}, mars_data, upsert=True)
     return redirect ('/', code=302)
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     app.run(debug=True)
